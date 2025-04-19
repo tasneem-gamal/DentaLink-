3662b7cbfce1e2565a55d8347ff6ee30c4a559ce
tasneem-gamal <tasneemgamal044@gmail.com>
2025-04-19 15:17:36 -0700

    create all tools repo

---
+++ lib/features/home/data/repo/all_tools/all_tools_repo.dart
@@ -0,0 +1,19 @@
+import 'package:dartz/dartz.dart';
+import 'package:dentalink/core/networking/api_error_handler.dart';
+import 'package:dentalink/features/home/data/apis/all_tools_service/all_tools_api_service.dart';
+import 'package:dentalink/features/home/data/models/all_tools/all_tools_response_body.dart';
+
+class AllToolsRepo {
+  final AllToolsApiService allToolsApiService;
+
+  AllToolsRepo(this.allToolsApiService);
+
+  Future<Either<Failure, AllToolsResponseBody>> getAllPatients() async{
+    try{
+      final response = await allToolsApiService.allToolsService();
+      return right(response);
+    } catch (e){
+      return left(ServerFailure(e.toString()));
+    }
+  }
+}
