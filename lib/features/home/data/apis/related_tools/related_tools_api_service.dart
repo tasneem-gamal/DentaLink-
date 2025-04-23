051c89eaf46dd476031a646cc322440459b3c213
tasneem-gamal <tasneemgamal044@gmail.com>
2025-04-22 19:58:10 -0700

    create related tools api service

--- lib/features/home/data/apis/related_tools/related_tools_api_service.dart
+++ lib/features/home/data/apis/related_tools/related_tools_api_service.dart
@@ -1,4 +1,8 @@
+import 'package:dentalink/core/helpers/constants.dart';
+import 'package:dentalink/core/helpers/shared_preference.dart';
+import 'package:dentalink/core/networking/api_constants.dart';
 import 'package:dentalink/core/networking/dio_factory.dart';
+import 'package:dentalink/features/home/data/models/related_tools/related_tools_response_body.dart';
 import 'package:dio/dio.dart';
 
 class RelatedToolsApiService {
@@ -7,6 +11,21 @@ class RelatedToolsApiService {
     dio = DioFactory.getDio();
   }
 
-  
+  Future<RelatedToolsResponseBody> relatedToolsService({required String toolId}) async{
+    try{
+      final response = await dio.get(
+        '${ApiConstants.baseUrl}tools/$toolId/related',
+        options: Options(
+          headers: {
+            'Authorization': 'Bearer ${await SharedPreferenceHelper.getSecuredString(SharedPreferencesKeys.userToken)}',
+          },
+        ),
+      );
+      return RelatedToolsResponseBody.fromJson(response.data);
+
+    } catch (e){
+      throw Exception('Failed to load data: $e');
+    }
+  }
 
 }
