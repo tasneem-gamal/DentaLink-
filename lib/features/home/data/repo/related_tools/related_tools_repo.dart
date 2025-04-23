42f419453a1dd97730f3e7c71637fe5b4cc6634b
tasneem-gamal <tasneemgamal044@gmail.com>
2025-04-22 20:00:51 -0700

    create related tools repo

---
+++ lib/features/home/data/repo/related_tools/related_tools_repo.dart
@@ -0,0 +1,22 @@
+import 'package:dartz/dartz.dart';
+import 'package:dentalink/core/networking/api_error_handler.dart';
+import 'package:dentalink/features/home/data/apis/related_tools/related_tools_api_service.dart';
+import 'package:dentalink/features/home/data/models/related_tools/related_tools_response_body.dart';
+
+class RelatedToolsRepo {
+  final RelatedToolsApiService relatedToolsApiService;
+
+  RelatedToolsRepo(this.relatedToolsApiService);
+
+  Future<Either<Failure, RelatedToolsResponseBody>> getRelatedTools(
+      {required String toolId}) async {
+    try {
+      final response = await relatedToolsApiService.relatedToolsService(toolId: toolId);
+      
+      return right(response);
+    } catch (e) {
+      
+      return left(ServerFailure(e.toString()));
+    }
+  }
+}
