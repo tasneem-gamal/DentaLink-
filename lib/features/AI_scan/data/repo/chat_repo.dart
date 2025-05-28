76e9e6a25238d6af41762a41a67e246232a920fd
tasneem-gamal <tasneemgamal044@gmail.com>
2025-05-28 11:16:26 -0700

    create chat repo

---
+++ lib/features/AI_scan/data/repo/chat_repo.dart
@@ -0,0 +1,26 @@
+import 'package:dartz/dartz.dart';
+import 'package:dentalink/core/networking/api_error_handler.dart';
+import 'package:dentalink/features/AI_scan/data/api/chat_api_service.dart';
+import 'package:dentalink/features/AI_scan/data/models/chat_request_body.dart';
+import 'package:dentalink/features/AI_scan/data/models/chat_response_body.dart';
+import 'package:dio/dio.dart';
+
+class ChatRepo {
+  final ChatApiService chatApiService;
+
+  ChatRepo(this.chatApiService);
+
+  Future<Either<Failure, ChatResponseBody>> sendMessageRepo(
+    ChatRequestBody chatRequestBody
+  ) async{
+    try{
+      final response = await chatApiService.sendMessage(chatRequestBody);
+      return right(response);
+    } catch (e){
+      if(e is DioException){
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure('An unexpected error occurred. Please try again.'));
+    }
+  }
+}
