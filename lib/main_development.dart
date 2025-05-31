007960a6a894a1dc386f24777c7553407e22f479
tasneem-gamal <tasneemgamal044@gmail.com>
2025-05-30 21:28:57 -0700

    refactor

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -4,6 +4,8 @@ import 'package:dentalink/core/helpers/shared_preference.dart';
 import 'package:dentalink/core/routing/app_router.dart';
 import 'package:dentalink/dentalink.dart';
 import 'package:dentalink/features/home/logic/theme_provider/theme_provider.dart';
+import 'package:dentalink/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
@@ -11,6 +13,9 @@ import 'package:provider/provider.dart';
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await ScreenUtil.ensureScreenSize();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await checkIfLoggedInUser();
   setUpGetIt();
   runApp(
